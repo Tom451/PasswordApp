86672cc1a246a6500bdc4cdd69a4888d898513e5
Tom451 <57364342+Tom451@users.noreply.github.com>
2020-10-19 19:00:31 +0100

    Simple Password Check - Check if length is over/under 8 Char

--- src/PasswordApp.java
+++ src/PasswordApp.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PasswordApp extends JFrame {
 
@@ -7,11 +9,24 @@ public class PasswordApp extends JFrame {
     private JTabbedPane tabbedPane1;
     private JPanel tabHome;
     private JPanel tabPasswordGenerator;
+    private JTextField passwordInput;
+    private JLabel passwordPageHeader;
+    private JButton enterButton;
+    private JButton cancelButton;
     private JTabbedPane tabbedMainPanel;
 
-    public static void setTabbedMainPanel(){
+    //getters and setters for main panel
+    public String getPasswordInput(){
+
+        return passwordInput.getText();
+    }
+
+    public static void main(String[] args){
+        PasswordApp page = new PasswordApp();
+        page.setVisible(true);
 
     }
+
 
     public PasswordApp() {
 
@@ -21,17 +36,29 @@ public class PasswordApp extends JFrame {
         pack();
 
 
+        enterButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
+                passwordCheck(getPasswordInput());
+
+
+            }
+        });
     }
 
+    public void passwordCheck(String input){
 
-
-    public static void main(String[] args){
-        PasswordApp page = new PasswordApp();
-        page.setVisible(true);
-
-
-
+        if(input.length() < 8){
+            JOptionPane.showMessageDialog(mainPanel,
+                    "Your Password is not secure",
+                    "Unsecure Password",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+        else{
+            JOptionPane.showMessageDialog(mainPanel,
+                    "Your Password is secure");
+        }
 
     }
 
